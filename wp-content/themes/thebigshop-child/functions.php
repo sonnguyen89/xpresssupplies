89bf1d5a6acda6ced371895342360d1e8290d33e
aussieadventurecaravans <son@aussieadventurecaravans.com.au>
2018-09-17 22:10:30 +1000

    update style of product category display at homepage

--- wp-content/themes/thebigshop-child/functions.php
+++ wp-content/themes/thebigshop-child/functions.php
@@ -5,3 +5,10 @@ wp_enqueue_style('thebigshop-parent-style', get_parent_theme_file_uri('style.css
 }
 
 add_action('wp_enqueue_scripts', 'thebigshop_child_scripts',20);
+
+
+add_filter('subcategory_archive_thumbnail_size','customize_subcategory_archive_thumbnail_size');
+function customize_subcategory_archive_thumbnail_size()
+{
+ return 'thebigshop_featured_image';
+}
